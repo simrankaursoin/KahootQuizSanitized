1babf6a5f298e79320a5ed8efb7dc36a97d933af
simransoin <simranksoin@gmail.com>
2019-06-17 11:08:26 -0700

    Release questions separately from answer messages on server side

--- quiz/consumers.py
+++ quiz/consumers.py
@@ -8,14 +8,21 @@ class QuizConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'quiz_%s' % self.room_name
-
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
-
         self.accept()
+
+        question = "new_question!"
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'question_message',
+                'question': question,
+            }
+        )
 
     def disconnect(self, close_code):
         # Leave room group
@@ -28,15 +35,12 @@ class QuizConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        question = "new_question!"
-
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'quiz_message',
                 'message': message,
-                'question': question,
             }
         )
 
@@ -47,53 +51,12 @@ class QuizConsumer(WebsocketConsumer):
         # Send message to WebSocket
         self.send(text_data=json.dumps({
             'message': message,
-            'question': event['question'],
         }))
 
+    def question_message(self, event):
+        question = event['question']
 
-# from channels.generic.websocket import AsyncWebsocketConsumer
-# import json
-#
-#
-# class QuizConsumer(AsyncWebsocketConsumer):
-#     async def connect(self):
-#         self.room_name = self.scope['url_route']['kwargs']['room_name']z
-#         self.room_group_name = 'quiz_%s' % self.room_name
-#
-#         # Join room group
-#         await self.channel_layer.group_add(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-#
-#         await self.accept()
-#
-#     async def disconnect(self, close_code):
-#         # Leave room group
-#         await self.channel_layer.group_discard(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-#
-#     # Receive message from WebSocket
-#     async def receive(self, text_data):
-#         text_data_json = json.loads(text_data)
-#         message = text_data_json['message']
-#
-#         # Send message to room group
-#         await self.channel_layer.group_send(
-#             self.room_group_name,
-#             {
-#                 'type': 'quiz_message',
-#                 'message': message
-#             }
-#         )
-#
-#     # Receive message from room group
-#     async def quiz_message(self, event):
-#         message = event['message']
-#
-#         # Send message to WebSocket
-#         await self.send(text_data=json.dumps({
-#             'message': message
-#         }))
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'question': question,
+        }))
